5d4754e5c4eb97be8a030b75809687fc2b339982
MUSTAFA <unlessuser99@gmail.com>
2024-09-07 20:24:13 +0530

    added forms preview for a default resume

--- resume/urls.py
+++ resume/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path('create/', views.create_resume_view, name='create_resume'),
     path('templates/', views.resume_templates, name='resume_templates'),
-    path('editor/<str:template_name>/', views.editor_template, name='editor_template'),
+    # path('editor/<str:template_name>/', views.editor_template, name='editor_template'),
 ]
--- resume/views.py
+++ resume/views.py
@@ -4,15 +4,6 @@ from django.contrib.auth.decorators import login_required
 from .forms import ResumeForm
 from .models import Resume
 
-
-def editor_template(request, template_name):
-    # Assuming you might have some logic to handle different templates
-    context = {'template_name': template_name}
-    return render(request, 'resume/editor_template.html', context)
-
-def resume_templates(request):
-    return render(request, 'NextGenCV/ResumeTemplates.html')
-
 @login_required
 def create_resume_view(request):
     # Check if the user already has a resume
@@ -25,12 +16,13 @@ def create_resume_view(request):
             resume = form.save(commit=False)
             resume.user = request.user  # Ensure the resume is linked to the logged-in user
             resume.save()
-            return redirect('dashboard')  # Redirect to the named URL pattern for the dashboard
-        else:
-            # Handle form errors if needed
-            return render(request, 'resume/create_resume.html', {'form': form})
+            return redirect('dashboard')  # Redirect to the dashboard after saving
     else:
         # Create a form with existing data if available, otherwise an empty form
         form = ResumeForm(instance=existing_resume)
 
     return render(request, 'resume/create_resume.html', {'form': form})
+
+def resume_templates(request):
+    # View to display available resume templates
+    return render(request, 'NextGenCV/ResumeTemplates.html')
